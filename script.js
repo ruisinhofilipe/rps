837a4386363486aa71408e01c9968083be2fca1c
Rui Silva <ruisinhofilipe@gmail.com>
2022-03-30 16:53:43 +0200

    Mod of pretty much everything, still need a few mods (still using console log to show what whas chose in each round

--- script.js
+++ script.js
@@ -1,40 +1,69 @@
+let playerScore = 0;
+let robotScore = 0;
+let tiesScore = 0;
 
-function startGame(){
-    let imgs = document.querySelectorAll("img")
-    imgs.forEach((img) => 
-        img.addEventListener(("click"), () => {
-            if(img.id){
-            playRound(img.id, computerSelection);
-            }
+// Robot selection between ROCK PAPER OR SCISSORS
+function robotPlay(){
+    let robotTurn = ["ROCK","PAPER", "SCISSORS"][Math.floor(Math.random() * 3)];
+    return robotTurn;
+}
+
+
+function game(){
+    let imgs = document.querySelectorAll(".container-options img");
+    imgs.forEach(img => {
+        img.addEventListener("click", () => {
+        playerChoice = img.id;
+        const robotChoice = robotPlay();
+        playRound(playerChoice, robotChoice);
+
+        // live update of the scores according to the games
+        document.querySelector(".player-score").textContent = "Human: " +  playerScore;
+        document.querySelector(".robot-score").textContent = "Robot: " +  robotScore;
+        document.querySelector(".ties-score").textContent = "ties: " +  tiesScore;
+        // document.querySelector(".round-p").textContent = "Result: " +  playRound(playerChoice, robotChoice);
+
+        controllWins();
         })
-    );
+    });
 }
 
-function computerPlay(){
-    let computerTurn = ["ROCK","PAPER", "SCISSORS"][Math.floor(Math.random() * 3)];
-    return computerTurn;
-}
+//outcome of each round played
 
-const computerSelection = computerPlay();
+function playRound(playerSelection, robotSelection){
+let win = "You win!" + " You selected " + playerSelection + " and the robot selected " + robotSelection + "."
+let lose = "You lost!" + " You selected " + playerSelection + " and the robot selected: " + robotSelection + "."
+let draw = "That's a draw!" + " You selected " + playerSelection + " and the robot selected " + robotSelection + "."
 
-function playRound(playerSelection,computerSelection){
-    if(
-        playerSelection == "ROCK" && computerSelection == "SCISSORS" ||
-        playerSelection == "PAPER" && computerSelection == "ROCK" ||
-        playerSelection == "SCISSORS" && computerSelection == "PAPER"
-      ){  
-        console.log("Player won! " + "You chose: " + playerSelection + ". Computer went with: " + computerSelection);        
-    }else if (
-        playerSelection == computerSelection
-    ){
-        console.log("Draw!" + " You chose: " + playerSelection + ". Computer went with: " + computerSelection);
+    if(playerSelection === robotSelection){
+        tiesScore++;
+        console.log(draw);
+    }else if((playerSelection === "ROCK") && (robotSelection === "PAPER")){
+        robotScore++;
+        console.log(lose);
+    }else if((playerSelection === "ROCK") && (robotSelection === "SCISSORS")){
+        playerScore++;
+        console.log(win);
+    }else if((playerSelection === "PAPER") && (robotSelection === "SCISSORS")){
+        robotScore++;
+        console.log(lose);
+     }else if((playerSelection === "PAPER") && (robotSelection === "ROCK")){
+        playerScore++;
+        console.log(win);
+    }else if((playerSelection === "SCISSORS") && (robotSelection === "PAPER")){
+        playerScore++;
+        console.log(win);
     }else{
-        console.log("Computer Won." + " You chose: " + playerSelection + ". Computer went with: " + computerSelection);
+        robotScore++;
+        console.log(lose);
     }
 }
 
-startGame();
-
-
-
-
+function controllWins(){
+    if(playerScore === 5){
+        alert("You Won!")
+    }else if(robotScore === 5){
+        alert("Robot Won!")
+    }
+}
+game();
